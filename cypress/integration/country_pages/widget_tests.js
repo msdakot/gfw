91a12997d83937684baa56503238a6e71c95f862
Benjamin A. Laken <benlaken@gmail.com>
2018-04-19 16:58:18 +0200

    currently not working

--- cypress/integration/country_pages/widget_tests.js
+++ cypress/integration/country_pages/widget_tests.js
@@ -1,20 +1,24 @@
-describe('Extent widget', function() {
+describe('Extent widget', () => {
   it('Checks extent in the Brazil country page has 3 categories by default', function() {
     cy.visit('/country/BRA');
+    cy.log('Test log message');
     cy
       .get('#treeCover')
       .find('li.legend-item')
       .should('have.length', 3);
   });
   it('Checks the area extent values shown sum to the area of the country', function() {
+    let total = 0;
     cy
       .get('#treeCover')
-      .find('li.legend-item')
-      .children('div.legend-value')
-      .then(item => {
-        //console.log('things', item.get('textContent'));
-        // want to iterate over text in 'textContent', seperate the values from units,
-        // sum, and compare to the known area of Brazil (with a tolerance)
+      .find('.legend-value')
+      .each(el => {
+        cy.log(el);
+        total += parseInt(el.innerHTML);
+        cy.log('Int parse', parseInt(el.innerHTML));
+      })
+      .then($elList => {
+        expect(total).to.be.closeTo(850, 10);
       });
   });
 });
