077630167cab1b6a6828f0a62bdb1b5d4d860b2e
Benjamin A. Laken <benlaken@gmail.com>
2018-04-19 15:10:37 +0200

    added first simple country page tests

---
+++ cypress/integration/country_pages/widget_tests.js
@@ -0,0 +1,18 @@
+describe('Extent widget', function() {
+  it('Checks extent in the Brazil country page has 3 categories by default', function() {
+    cy.visit('/country/BRA');
+    cy
+      .get('#treeCover')
+      .find('li.legend-item')
+      .should('have.length', 3);
+  });
+  it('Checks the area extent values shown sum to the area of the country', function() {
+    cy
+      .get('#treeCover')
+      .find('li.legend-item')
+      .children('div.legend-value')
+      .then(item => {
+        //console.log('things', item.get('textContent'));
+      });
+  });
+});
